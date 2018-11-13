73e164c39d860990dc73461329f2ae776e273947
ios dev <vorobyev.semyon@gmail.com>
2018-11-13 17:25:09 +0300

    Login animation done

--- AnimationsExample/ViewController.swift
+++ AnimationsExample/ViewController.swift
@@ -8,13 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //MARK: Constants
+    private var viewHeight: CGFloat = 0
+    private let startAlpha:CGFloat = 0
+    private let endAlpha:CGFloat = 1
+    private let animationTime = 1.5
+    
+    //MARK: Outlets
+    @IBOutlet private weak var heyLabel: UILabel!
+    @IBOutlet private weak var youLabel: UILabel!
+    @IBOutlet private weak var preetyLabel: UILabel!
+    @IBOutlet private weak var loginButton: UIButton!
+    
+    //MARK: Lifecycle
+    override func viewDidAppear(_ animated: Bool) {
+        viewHeight = view.bounds.height
+        setUpPreparation()
+        startAnimationsQueue()
     }
-
-
+    
+    private func setUpPreparation() {
+        heyLabel.transform = CGAffineTransform(translationX: 0, y: viewHeight)
+        
+        heyLabel.alpha = startAlpha
+        youLabel.alpha = startAlpha
+        preetyLabel.alpha = startAlpha
+        loginButton.alpha = startAlpha
+    }
+    
+    private func startAnimationsQueue() {
+        UIView.animate(withDuration: animationTime, animations: { [weak self] in
+            guard let `self` = self else { return }
+            self.heyLabel.transform = CGAffineTransform.identity
+            self.heyLabel.alpha = self.endAlpha
+        }, completion: youLabelAlpha)
+    }
+    
+    private func youLabelAlpha(_ ended:Bool) {
+        guard ended else {return}
+        UIView.animate(withDuration: animationTime, animations: { [weak self] in
+            guard let `self` = self else { return }
+            self.youLabel.alpha = self.endAlpha
+            self.preetyLabelAlpha()
+            }, completion: nil)
+    }
+    
+    private func preetyLabelAlpha () {
+        UIView.animate(withDuration: animationTime, delay: 0.9, options: [], animations: { [weak self] in
+            guard let `self` = self else { return }
+            self.preetyLabel.alpha = self.endAlpha
+            }, completion: buttonAlpha)
+    }
+    
+    private func buttonAlpha (_ ended: Bool) {
+        guard ended else {return}
+        UIView.animate(withDuration: animationTime, animations: { [weak self] in
+            guard let `self` = self else { return }
+            self.loginButton.alpha = self.endAlpha
+            }, completion: nil)
+    }
+    
 }
 
